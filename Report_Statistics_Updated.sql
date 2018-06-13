a4f587d44907c198edf619935c37f04a74235041
1512001 <38588673+1512001@users.noreply.github.com>
2018-06-13 16:15:04 +0700

    Update Report_Statistics_Updated.sql

--- Report_Statistics_Updated.sql
+++ Report_Statistics_Updated.sql
@@ -42,47 +42,23 @@ END
 
 -- Revenue Report
 GO
-GO
-
--- Revenue Report
 ALTER PROCEDURE SP_RevenueReport (@option NVARCHAR(10),@hotelname NVARCHAR(50), @m INT, @y INT)
 AS BEGIN
 DECLARE @sqlstring nvarchar(1500), @input nvarchar(100)
 IF(@option = N'Tháng') --báo cáo doanh thu theo tháng
 	BEGIN
-		IF(@m IS NOT NULL AND @y IS NULL) ---chỉ theo tháng (cho trước)
-
-		BEGIN
-			SET @sqlstring = 'SELECT K.maKS, K.tenKS, DATEPART(MONTH,H.ngayThanhToan) as Thang, SUM(H.tongTien) as DoanhThu
+	SET @sqlstring = 'SELECT K.maKS, K.tenKS, DATEPART(MONTH,H.ngayThanhToan) as Thang, SUM(H.tongTien) as DoanhThu
 						      FROM HoaDon H, DatPhong D, LoaiPhong L, KhachSan K
-						      WHERE K.maKS=L.MaKS AND L.maLoaiPhong=D.maLoaiPhong AND D.maDP=H.maDP AND DATEPART(MONTH,H.ngayThanhToan) = @m'
-			---IF(@hotelname IS NULL) ---không báo cáo theo khách sạn --không làm gì cả
-			IF(@hotelname IS NOT NULL)---báo cáo theo khách sạn
-				SET @sqlstring = @sqlstring  + ' AND K.tenKS like N''%' +@hotelname + '%'''
-			---thêm phần group by và order
-			SET @sqlstring = @sqlstring + '  GROUP BY K.maKS, K.tenKS, DATEPART(MONTH,H.ngayThanhToan)'
-			--SET @sqlstring = @sqlstring + ' ORDER BY K.maKS'
-		END
-
-		ELSE ---theo tháng, năm
-
-		BEGIN
-			SET @sqlstring = 'SELECT K.maKS, K.tenKS, DATEPART(MONTH,H.ngayThanhToan) as Thang, DATEPART(YEAR,H.ngayThanhToan) as Nam,SUM(H.tongTien) as DoanhThu
-						      FROM HoaDon H, DatPhong D, LoaiPhong L, KhachSan K
-						      WHERE  K.maKS=L.MaKS AND L.maLoaiPhong=D.maLoaiPhong AND D.maDP=H.maDP '
-			---IF(@hotelname IS NULL) ---không báo cáo theo khách sạn --không làm gì cả
-			IF(@hotelname IS NOT NULL)---báo cáo theo khách sạn
-				SET @sqlstring = @sqlstring + ' AND K.tenKS LIKE N''%' +@hotelname + '%'''
-			---IF(@m IS NULL AND @y IS NULL) ---tháng bất kỳ, năm bất kỳ ---không làm gì hết
-			IF(@m IS NULL AND @y IS NOT NULL) ---tháng bất kỳ, năm cho trước
-				SET @sqlstring = @sqlstring + ' AND DATEPART(YEAR,H.ngayThanhToan) = @y'	
-			IF(@m IS NOT NULL AND @y IS NOT NULL) ---tháng cho trước, năm cho trươc
-				SET @sqlstring = @sqlstring + ' AND DATEPART(YEAR,H.ngayThanhToan) = @y AND DATEPART(MONTH,H.ngayThanhToan) = @m'
-			---thêm phần group by và order
-			SET @sqlstring = @sqlstring + ' GROUP BY K.maKS, K.tenKS, DATEPART(MONTH,H.ngayThanhToan), DATEPART(YEAR,H.ngayThanhToan) '
-			--SET @sqlstring = @sqlstring + ' ORDER BY K.maKS'
-		END
-
+						      WHERE K.maKS=L.MaKS AND L.maLoaiPhong=D.maLoaiPhong AND D.maDP=H.maDP '
+		---IF(@hotelname IS NULL) ---không báo cáo theo khách sạn --không làm gì cả
+		IF(@hotelname IS NOT NULL)---báo cáo theo khách sạn
+		SET @sqlstring = @sqlstring + ' AND K.tenKS LIKE N''%' +@hotelname + '%'''
+		---IF(@m IS NULL) ---không làm gì cả
+		IF(@m is NOT NULL)
+		SET @sqlstring = @sqlstring + ' AND DATEPART(MONTH,H.ngayThanhToan) = @m'
+		--thêm group by và order
+		SET @sqlstring = @sqlstring + ' GROUP BY DATEPART(MONTH,H.ngayThanhToan), K.maKS, K.tenKS'
+		--SET @sqlstring = @sqlstring + ' ORDER BY K.maKS'
 	END
 
 ELSE IF((@option = N'Năm' AND @m IS NULL AND @y IS NOT NULL)
@@ -103,9 +79,27 @@ BEGIN
 	--SET @sqlstring = @sqlstring + ' ORDER BY K.maKS'
 END
 
+ELSE IF (@option=N'Tháng, Năm')
+BEGIN
+	SET @sqlstring = 'SELECT K.maKS, K.tenKS, DATEPART(MONTH,H.ngayThanhToan) as Thang, DATEPART(YEAR,H.ngayThanhToan) as Nam,SUM(H.tongTien) as DoanhThu
+						      FROM HoaDon H, DatPhong D, LoaiPhong L, KhachSan K
+						      WHERE  K.maKS=L.MaKS AND L.maLoaiPhong=D.maLoaiPhong AND D.maDP=H.maDP '
+	---IF(@hotelname IS NULL) ---không báo cáo theo khách sạn --không làm gì cả
+	IF(@hotelname IS NOT NULL)---báo cáo theo khách sạn
+		SET @sqlstring = @sqlstring + ' AND K.tenKS LIKE N''%' +@hotelname + '%'''
+	---IF(@m IS NULL AND @y IS NULL) ---tháng bất kỳ, năm bất kỳ ---không làm gì hết
+	IF(@m IS NULL AND @y IS NOT NULL) ---tháng bất kỳ, năm cho trước
+		SET @sqlstring = @sqlstring + ' AND DATEPART(YEAR,H.ngayThanhToan) = @y'	
+	IF(@m IS NOT NULL AND @y IS NOT NULL) ---tháng cho trước, năm cho trươc
+		SET @sqlstring = @sqlstring + ' AND DATEPART(YEAR,H.ngayThanhToan) = @y AND DATEPART(MONTH,H.ngayThanhToan) = @m'
+	---thêm phần group by và order
+		SET @sqlstring = @sqlstring + ' GROUP BY K.maKS, K.tenKS, DATEPART(MONTH,H.ngayThanhToan), DATEPART(YEAR,H.ngayThanhToan) '
+		--SET @sqlstring = @sqlstring + ' ORDER BY K.maKS'
+END
+
 ELSE
 BEGIN
-	RAISERROR (N'Lỗi! Không thể báo cáo doanh thu theo năm theo định dạng này. Báo cáo theo năm phải có tháng là rỗng',16,1);
+	RAISERROR (N'Lỗi! Không đúng định dạng',16,1);
 	RETURN
 END
 SET @input = ' @option NVARCHAR(10), @hotelname NVARCHAR(50), @m INT, @y INT '
